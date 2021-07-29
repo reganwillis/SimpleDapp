fe8d44f72510e21292770e33f6fd569da95644bc
Regan Willis <reganwillis@outlook.com>
2021-07-29 13:07:18 -0500

    blockchain storage accessed through frontend

--- public/bundle.js
+++ public/bundle.js
@@ -1,35 +1,102 @@
-var contractABI = [
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "hello",
-    "outputs": [
+const contractABI = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "data",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x73d4a13a"
+      },
       {
-        "name": "",
-	"type": "string"
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_data",
+            "type": "string"
+          }
+        ],
+        "name": "set_data",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        "signature": "0x6feab03b"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "get_data",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x50bf8b0d"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "hello",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "pure",
+        "type": "function",
+        "signature": "0x19ff1d21"
       }
-    ],
-    "payable": false,
-    "stateMutability": "pure",
-    "type": "function",
-    //"signature": "0x19ff1d21"
-  }
 ];
+
 // get value from hello world contract deployment
-var contractAddress = '0x80b7A8154fa3a37D9bE6C2947366AdC57702c032';
+const contractAddress = '0x362de7c64d6E1c6A138Bd90c48Ed96e82AFB1Dce';
+
 // create instance of web3
 // get address from blockchain running ganache
-var web3 = new Web3('http://localhost:8545');
+const web3 = new Web3('http://localhost:8545');
 
 // build smart contract object with web3
 // pointer to a hello world smart contract
-var helloWorld = new web3.eth.Contract(contractABI, contractAddress);
+const helloWorld = new web3.eth.Contract(contractABI, contractAddress);
 
 // wait for document content to load
 document.addEventListener('DOMContentLoaded', () => {
-  // access smart contract hello method
-  helloWorld.methods.hello().call().then(result => {
-    document.getElementById('hello').innerHTML = result;
+
+  const $setData = document.getElementById('setData');
+  const $data = document.getElementById('data');
+  let accounts = [];
+
+  web3.eth.getAccounts().then(_accounts => {
+    accounts = _accounts;
+  });
+
+  const getData = () => {
+    helloWorld.methods.get_data().call().then(result => {
+      $data.innerHTML = result;
+    });
+  };
+
+  getData();
+
+  $setData.addEventListener('submit', e => {
+    e.preventDefault();  // prevent page from reloading after submit
+    const data = e.target.elements[0].value;
+
+    // send transaction to blockchain
+    helloWorld.methods.set_data(data).send({from: accounts[0]}).then(getData);
   });
 });
 
