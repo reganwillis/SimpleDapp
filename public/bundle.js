9e810b09e833f168c8f368d321cb97d3f2eb2dc0
Regan Willis <reganwillis@outlook.com>
2021-07-29 10:04:50 -0500

    access smart contract method from frontend

--- public/bundle.js
+++ public/bundle.js
@@ -1,13 +1,37 @@
-var contractABI = [];
-// get value from contact deployment
-var contractAddress = '0x36009B38eBFd82DEab2B7C36094FcB928F4cd5d0';
-
+var contractABI = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "hello",
+    "outputs": [
+      {
+        "name": "",
+	"type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "pure",
+    "type": "function",
+    //"signature": "0x19ff1d21"
+  }
+];
+// get value from hello world contract deployment
+var contractAddress = '0x80b7A8154fa3a37D9bE6C2947366AdC57702c032';
 // create instance of web3
 // get address from blockchain running ganache
 var web3 = new Web3('http://localhost:8545');
 
 // build smart contract object with web3
-var simpleSmartContract = new web3.eth.Contract(contractABI, contractAddress);
+// pointer to a hello world smart contract
+var helloWorld = new web3.eth.Contract(contractABI, contractAddress);
 
-console.log(simpleSmartContract);
+// wait for document content to load
+document.addEventListener('DOMContentLoaded', () => {
+  // access smart contract hello method
+  helloWorld.methods.hello().call().then(result => {
+    document.getElementById('hello').innerHTML = result;
+  });
+});
+
+console.log(helloWorld);
 web3.eth.getAccounts().then(console.log);
