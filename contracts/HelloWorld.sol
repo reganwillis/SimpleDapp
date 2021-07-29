63b50637544bd3f006bfbd0555020bf68774e788
Regan Willis <reganwillis@outlook.com>
2021-07-29 11:48:39 -0500

    adding and testing transaction in smart contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,6 +1,21 @@
 pragma solidity ^0.5.0;
 
 contract HelloWorld {
+    // variables defined outside functions will automatically
+    // be saved in 'storage' - on the blockchain
+    string public data;
+
+    // set data
+    function set_data(string memory _data) public {
+        data = _data;
+    }
+
+    // get data
+    function get_data() view public returns(string memory) {
+        return data;
+    }
+
+    // return a string
     function hello() pure public returns(string memory) {
         return 'Hello World';
     }
--- test/helloWorld.js
+++ test/helloWorld.js
@@ -2,6 +2,15 @@
 const HelloWorld = artifacts.require('HelloWorld');
 
 contract('HelloWorld', () => {
+
+  it('sets value of data variable', async () => {
+    const helloWorld = await HelloWorld.deployed();
+    var data = 'test adding data';
+    await helloWorld.set_data(data);
+    const result = await helloWorld.get_data();
+    assert(result === data);
+  });
+
   it('returns text: hello world', async () => {
     const helloWorld = await HelloWorld.deployed();
     const result = await helloWorld.hello();
